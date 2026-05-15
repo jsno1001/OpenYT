5abe28e4e275e524c68e42b048259928840678e6
jsno1001 <jsno5555@gmail.com>
2026-05-15 16:06:24 +0300

    Added logo

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+const path = require("path")
 const express = require("express");
 
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello OpenYT</h1>');
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(port, () => { console.log("Server started on http://localhost:"+port) });
