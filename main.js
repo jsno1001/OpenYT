99901575fb2187b59ae47a165d1f0ed604fdcd74
jsno1001 <jsno5555@gmail.com>
2026-05-15 16:22:20 +0300

    Added static pages

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
