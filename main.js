00144eb00266b8f876b824fc7d05e178a8a8e557
jsno1001 <jsno5555@gmail.com>
2026-05-17 21:03:28 +0300

    dfsdfasdf

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+const videos = path.join(__dirname, "public/videos")
+
+app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    // res.sendFile(path.join(__dirname, "public/index.html"));
+    res.render("index", {
+        logo: "logo.html"
+    });
 });
 
 app.listen(port, () => { console.log("Server started on http://localhost:"+port) });
